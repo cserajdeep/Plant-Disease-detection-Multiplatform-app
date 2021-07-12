349e34ec6bb5b2f2488637ca0a23608804c73ee9
Dr. Rajdeep Chatterjee <cse.rajdeep@gmail.com>
2021-07-12 22:25:44 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Plant-Disease-detection-Multiplatform-app
+Offline classification of plant diseases using pretrained deep learning model
+
+### Link to collab notebook
+https://colab.research.google.com/drive/1Wka71E7O_e_PJ-RnJsEyqlLzNhYvAFrZ?usp=sharing
+
+### Video Walkthrough code
+https://drive.google.com/file/d/1p42I9oax6lrnys4tN0fcp3lBUY2P05xN/view?usp=sharing
---
+++ src/model/classes.js
@@ -0,0 +1,18 @@
+//List of categories =  ['Potato___healthy', 'Pepper__bell___healthy', 'Pepper__bell___Bacterial_spot', 'Tomato_Bacterial_spot', 'Potato___Early_blight', 'Tomato_healthy', 'Tomato_Early_blight', 'Potato___Late_blight', 'Tomato_Late_blight', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato_Leaf_Mold', 'Tomato__Target_Spot', 'Tomato__Tomato_mosaic_virus', 'Tomato__Tomato_YellowLeaf__Curl_Virus']
+export const MODEL_CLASSES = {
+    0: 'Potato___healthy',
+    1: 'Pepper__bell___healthy',
+    2: 'Pepper__bell___Bacterial_spot',
+    3: 'Tomato_Bacterial_spot',
+    4: 'Potato___Early_blight',
+    5: 'Tomato_healthy',
+    6: 'Tomato_Early_blight',
+    7: 'Potato___Late_blight',
+    8: 'Tomato_Late_blight',
+    9: 'Tomato_Septoria_leaf_spot',
+    10: 'Tomato_Spider_mites_Two_spotted_spider_mite',
+    11: 'Tomato_Leaf_Mold',
+    12: 'Tomato__Target_Spot',
+    13: 'Tomato__Tomato_mosaic_virus',
+    14: 'Tomato__Tomato_YellowLeaf__Curl_Virus'
+  };
